1f5c4d58e903a9583b1c9733fb7eb103fc75a7ab
Fernando Pérez Holguín <perez.holguin@gmail.com>
2023-12-09 01:01:01 +0100

    Actualizar dia8p2.py

--- dia8/dia8p2.py
+++ dia8/dia8p2.py
@@ -22,7 +22,7 @@ with open("dia8.txt","r") as f:
   pasos_necesarios=[0]*len(lugares_actuales)
   while 0 in pasos_necesarios:
     for i,lugar in enumerate(lugares_actuales):
-      if lugar[-1]=="Z":
+      if lugar[-1]=="Z" and pasos_necesarios[i]==0:
         pasos_necesarios[i]=pasos
     if direcciones[pasos%len(direcciones)]=="L":
       for i,lugar in enumerate(lugares_actuales):
@@ -30,13 +30,5 @@ with open("dia8.txt","r") as f:
     else:
       for i,lugar in enumerate(lugares_actuales):
         lugares_actuales[i]=mapa[lugar][1]
-    pasos+=1
-  # lugar_actual="AAA"
-  # while lugar_actual!="ZZZ":
-  #   #print(mapa[lugar_actual],"y tengo que ir por",direcciones[pasos%len(direcciones)])
-  #   if direcciones[pasos%len(direcciones)]=="L":
-  #     lugar_actual=mapa[lugar_actual][0]
-  #   else:
-  #     lugar_actual=mapa[lugar_actual][1]
-  #   pasos+=1
+    pasos+=1
   print("Resultado:",lcm(*pasos_necesarios))
