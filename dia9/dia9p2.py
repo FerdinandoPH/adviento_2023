08e1001000eaceea400bd97b09b26d280775ac4b
FerdinandoPH <perez.holguin@gmail.com>
2023-12-10 15:38:34 +0100

    dia 9

--- dia9/dia9p2.py
+++ dia9/dia9p2.py
@@ -4,15 +4,15 @@ with open("dia9.txt","r") as f:
   for linea in lineas:
     diferencias=[]
     lista_valores=[int(i) for i in linea.split(" ")]
-    diferencias.append(lista_valores[-1])
+    diferencias.append(lista_valores[0])
     while lista_valores!=[0]*len(lista_valores):
       valores_temporales=[]
       for i in range(1,len(lista_valores),1):
         valores_temporales.append(lista_valores[i]-lista_valores[i-1])
       lista_valores=valores_temporales.copy()
-      diferencias.append(lista_valores[-1])
+      diferencias.append(lista_valores[0])
     suma_de_linea=0
-    for numero in diferencias:
-      suma_de_linea+=numero
+    for i,numero in enumerate(diferencias):
+      suma_de_linea+=numero*((-1)**i)
     suma+=suma_de_linea
   print("Resultado:",suma)
